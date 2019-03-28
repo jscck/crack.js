aacf025ac68459ae2849c5f984404b28a193749d
Terry Cai <gidcai@gmail.com>
2019-03-29 00:04:00 +0800

    更新注释

--- crack.js
+++ crack.js
@@ -1,14 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const getArgs = require('./getargs');
 const { spawn } = require('child_process');
-
-var _0x2c19 = []; // 这里是字典
-var _0x27c8 = function (x, c) {
-  x = x - 0;
-  var e = _0x2c19[x];
-  return e
-};
 
 var matchOperatorsRe = /[|\\{}()\[\]^$+*?\.]/g;
 var escapeRegString = function (str) {
@@ -18,24 +10,6 @@ var escapeRegString = function (str) {
 
   return str.replace(matchOperatorsRe, '\\$&');
 };
-
-var COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
-var DEFAULT_PARAMS = /=[^,]+/mg;
-var FAT_ARROWS = /=>.*$/mg;
-
-function getParameterNames(fn) {
-  var code = fn.toString()
-    .replace(COMMENTS, '')
-    .replace(FAT_ARROWS, '')
-    .replace(DEFAULT_PARAMS, '');
-
-  var result = code.slice(code.indexOf('(') + 1, code.indexOf(')'))
-    .match(/([^\s,]+)/g);
-
-  return result === null
-    ? []
-    : result;
-}
 
 
 
@@ -66,9 +40,17 @@ let methodSymbol = [
   'in'
 ];
 
+
+var _0x2c19 = []; // 这里是字典
+var _0x27c8 = function (x, c) {
+  x = x - 0;
+  var e = _0x2c19[x];
+  return e
+};
+
 var replaceVar = (str) => {
   var rules = [
-    {
+    { // 替换字典函数
       exec: () => {
         return new RegExp('_0x27c8\\("([a-z0-9]+)"\\)', 'g')
       },
@@ -79,6 +61,7 @@ var replaceVar = (str) => {
       }
     },
     ...repeat({
+      // 替换别名变量，预执行 如 r["hJymj"] = "HBOuD"; 所有匹配变量 变成 "HBOuD"
       match: () => {
         return new RegExp('[a-z_]\\["[a-zA-Z]+"\\]\\s+=\\s+"[\\s\\S]*?"', 'g')
       },
@@ -86,10 +69,8 @@ var replaceVar = (str) => {
         return new RegExp(escapeRegString(str) + '(?!\\s=)', 'g');
       }
     }, 20)];
-
   rules.forEach(function (item) {
-    
-    if (item.match) {
+    if (item.match) { // 替换别名变量
       let mas = str.match(item.match());
       let mqs = [];
       if (mas) {
@@ -112,8 +93,81 @@ var replaceVar = (str) => {
   return str;
 }
 
+/**
+ * 查询函数
+ * @param {*} str 字符串
+ * @param {*} regex 函数开始标志
+ * @param {array} splits 分隔符
+ * @param {*} callback 查询loop回调
+ */
+var searchScope = (str, funcstart, splits, callback) => {
+  let splitstart = splits[0];
+  let splitend = splits[1];
+  let match;
+  while (match = funcstart.exec(str)) {
+    let level = 0;
+    let left = match.index;
+    let right = 0;
+    let closed = false;
+    while (left = left + 1) {
+      if (str[left] == splitstart) {
+        level++;
+        closed = false;
+      }
+      if (str[left] == splitend) {
+        level--;
+        closed = true;
+      }
+      if (level == 0 && closed) {
+        right = left;
+        break;
+      }
+    }
+    callback({ match, funcstart, right });
+  }
+}
+
+/**
+ * 获取调用函数参数，返回数组
+ * @param {*} str 
+ * @example  
+ * ```
+ * 
+ *  t["eyXPW"]("object", t["eyXPW"](void 0, x) ? "undefined" : t["XUIJK"](l, x))
+ * 
+ *  返回 ["object", 't["eyXPW"](void 0, x) ? "undefined" : t["XUIJK"](l, x)']
+ * 
+ * ```
+ */
+var getArgs = (str) => {
+  let cloneStr = str;
+  let args = [];
+  let funcstart = new RegExp('([^\\(,\\s])+?(?=\\()', 'g');
+  searchScope(str, funcstart, ["(", ")"], function({ match, right}){
+    let current = str.slice(match.index, right + 1);
+    str = str.replace(current, repeat("*",current.length).join(''))
+  })
+  let spliter = /,\s?/g;
+  let start = 0;
+  let match;
+  while(match = spliter.exec(str)) {
+      args.push(cloneStr.slice(start, match.index))
+      // update split index
+      start = spliter.lastIndex;
+  }
+  // last substring
+  args.push(cloneStr.slice(start))
+  return args;
+}
 
 
+
+/**
+ * 查找函数对象
+ * @param {*} str 字符串
+ * @param {*} reg 函数开始标志
+ * @param {*} splits 分隔符
+ */
 var findFunc = (str) => {
   let funcstart = new RegExp('(\\b[a-z_]\\["[a-zA-Z]{5}"\\])\\s*=\\s*function', 'g');
   let match;
@@ -156,6 +210,10 @@ var findFunc = (str) => {
   return arr;
 }
 
+/**
+ * 替换函数
+ * @param {*} str 
+ */
 var replaceFunc = (str) => {
   let arrMix = findFunc(str);
   arrMix.forEach((reItem) => {
@@ -242,10 +300,11 @@ var removeFunc = (str) => {
   return str;
 }
 /**
- * 移除变量
+ * 移除变量 
  * @param {*} str 
+ * @example r["hJymj"] = "HBOuD";
  */
-var removeVar = (str)=> {
+var removeVar = (str) => {
   let funcstart = new RegExp('[a-z_]\\["[a-zA-Z]{5}"\\]\\s+=\\s+"[\\s\\S]*?"', 'g');
   let match;
   while (match = funcstart.exec(str)) {
@@ -263,7 +322,7 @@ var removeVar = (str)=> {
  * 移除空行
  * @param {*} str 
  */
-var removeEmptyLine = (str)=> {
+var removeEmptyLine = (str) => {
   let funcstart = /^\s*[\r\n]/gm;
   let match;
   while (match = funcstart.exec(str)) {
@@ -278,7 +337,7 @@ var removeEmptyLine = (str)=> {
  * @param {*} str 
  * @example Object["defineProperty"] => Object.defineProperty
  */
-var convertVar = (str)=> {
+var convertVar = (str) => {
   let funcstart = new RegExp('\\["[a-zA-Z_]+"\\]', 'g');
   let match;
   while (match = funcstart.exec(str)) {
@@ -292,33 +351,37 @@ var convertVar = (str)=> {
 
 
 
+function main() {
+  let sourcePath = 'source/bug-unpack.js';
+  let crackName = 'dist/crack-' + Date.now() + '.js';
 
-let sourcePath = '../source/unpack.js';
-let crackName = '../dist/crack-' + Date.now() + '.js';
+  let template = fs.readFileSync(path.resolve(__dirname, sourcePath), 'utf-8').toString();
+  repeat('', 2).forEach(() => {
+    template = replaceVar(template);
+  })
 
-var template = fs.readFileSync(path.resolve(__dirname, sourcePath), 'utf-8').toString();
-repeat('', 2).forEach(() => {
-  template = replaceVar(template);
-})
+  repeat('', 3).forEach(() => {
+    template = replaceFunc(template);
+  })
+  // 打扫现场
+  repeat('', 20).forEach(() => {
+    template = removeFunc(template);
+  })
+  repeat('', 20).forEach(() => {
+    template = removeVar(template);
+  })
 
-repeat('', 3).forEach(() => {
-  template = replaceFunc(template);
-})
+  template = removeEmptyLine(template);
 
-// 打扫现场
-repeat('', 20).forEach(() => {
-  template = removeFunc(template);
-})
-repeat('', 20).forEach(() => {
-  template = removeVar(template);
-})
+  // 转换变量
+  repeat('', 2).forEach(() => {
+    template = convertVar(template);
+  })
 
-template = removeEmptyLine(template);
+  fs.writeFileSync(path.resolve(__dirname, crackName), template);
 
-// 转换变量
-repeat('', 2).forEach(() => {
-  template = convertVar(template);
-})
+  spawn('prettier-eslint', ['--write', crackName]);
+};
 
-fs.writeFileSync(path.resolve(__dirname, crackName), template);
-spawn('prettier-eslint', ['--write', crackName]);
+
+main();
